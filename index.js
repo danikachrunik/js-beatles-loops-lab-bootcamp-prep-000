752f0ad16d368649a6597c05a87300d0bd1d1c9a
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-25 21:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   let i = 0;
-  while (count < facts.length) {
+  while (i < facts.length) {
     facts[i]+="!!!";
     i++;
   }
