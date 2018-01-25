262b3434c8e48e05c43857e3cfe9a17bf002a17b
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-25 21:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function johnLennonFacts(facts) {
   
 }
+
+function iLoveTheBeatles(num) {
+  var arr = {};
+  do {
+    array.push("I Love the Beatles!");
+    num++;
+  } while(num < 15);
+  return arr;
+}
